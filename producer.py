02178c0a89ec20c1bb868d7b6614d861bf0202d7
AsmaChloe <asmachloe@gmail.com>
2024-10-09 00:15:47 +0200

    First draft of order producer

--- producer.py
+++ producer.py
@@ -3,6 +3,9 @@ import random
 from kafka import KafkaProducer
 import json
 import pandas as pd
+from faker import Faker
+from datetime import datetime
+import random
 
 # Kafka Producer
 producer = KafkaProducer(
@@ -10,27 +13,71 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-topic = 'temperature-sensor'
-data_source_filename = "./data/sales_data.csv"
+topic = 'order-sensor'
 
-def load_data(filename=data_source_filename) :
-    df = pd.read_csv(filename, sep=',')
+def generate_order_id():
+    #TODO
+    return 0
 
-def produce_temperature_data():
-    sensor_id = random.randint(1, 100)  # Identifiant aléatoire pour un capteur
-    temperature = round(random.uniform(15.0, 30.0), 2)  # Température aléatoire
-    timestamp = time.time()  # Timestamp actuel
+def generate_order_data(customer_id):
+    order_id = generate_order_id() 
+    order_status_id = 0
+    customer_id = customer_id
+    order_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     return {
-        'sensor_id': sensor_id,
-        'temperature': temperature,
-        'timestamp': timestamp
+        "order_id" : order_id,
+        "order_status_id" : order_status_id,
+        "customer_id" : customer_id,
+        "order_date" : order_date
     }
 
+def pick_product() :
+    #TODO
+    product_data = {
+        "product_id" : 0,
+        "product_name" : "USB Cable",
+        "category_id" : 0,
+        "price" : 0.99,
+        "min_stock_threshold" : 10
+    }
+    return product_data
+
+def generate_product_order_id() :
+    #TODO
+    return 0
+
+def generate_product_order(customer_id) :
+
+    order_data = generate_order_data(customer_id)
+    order_id = order_data['order_id']
+
+    nb_product = random.randint(1,6)
+
+    product_order_data_list = []
+    for i in range(nb_product) :
+        product_data = pick_product()    
+        product_id = product_data['product_id']
+
+        quantity = random.randint(1,6) #TODO vérifier la limite des stocks
+
+        product_order_data = {
+            "product_order_id" : generate_product_order_id(),
+            "product_id" : product_id,
+            "order_id" : order_id,
+            "quantity" : quantity
+        }
+
+        product_order_data_list.append(product_order_data)
+    #TODO save product_order_data
+
+    return order_data
+
 try :
-    load_data()
 
     while True:
-        data = produce_temperature_data()
+        data = generate_product_order(0)
+
         print(f"Sending data : {data}")
 
         #Data sent every 30 secs => internal timeout of kafka producer
